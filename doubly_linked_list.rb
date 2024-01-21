9458ae024f1347ee82d8d3a6f545b955ff648410
kenderb <kenderbolivar@gmail.com>
2024-01-20 19:07:20 -0500

    Add DoublyLinkedList

---
+++ doubly_linked_list.rb
@@ -0,0 +1,102 @@
+# frozen_string_literal: true
+
+# Represents a node in a linked list.
+class Node
+  attr_accessor :data, :next, :prev
+
+  def initialize(data)
+    @data = data
+    @next = nil
+    @prev = nil
+  end
+end
+
+# Represents a doubly linked list data structure.
+class DoublyLinkedList
+  attr_accessor :head, :tail, :length
+
+  def initialize(value)
+    @head = Node.new(value)
+    @tail = @head
+    @length = 1
+  end
+
+  def append(data)
+    node = Node.new(data)
+    node.prev = @tail
+    @tail.next = node
+    @tail = node
+
+    @length += 1
+
+    draw
+  end
+
+  def draw
+    current = head
+    linked_list = []
+    arrow = '<-->'
+
+    while current
+      linked_list << "(#{current.data})"
+      current = current.next
+    end
+
+    "#{linked_list.join(arrow)}-->nil"
+  end
+
+  # def insert(index, value)
+  #   return prepend(value) if index.zero?
+  #   return append(value) if index + 1 == length || index + 1 >= length
+
+  #   node = Node.new(value)
+  #   current = head
+
+  #   current = traversal(current, index - 1)
+  #   swatch(current, node)
+  #   @length += 1
+
+  #   draw
+  # end
+
+  # def delete(index)
+  #   return draw if @length == 1
+
+  #   index = length - 1 if index > length
+  #   current = head
+
+  #   current = traversal(current, index - 1)
+  #   current.next = current.next.next if current.next
+  #   @length -= 1
+
+  #   draw
+  # end
+
+  # def prepend(data)
+  #   node = Node.new(data)
+  #   current = node
+  #   current.next = @head
+  #   @head = current
+  #   @length += 1
+
+  #   draw
+  # end
+
+  # private
+
+  # def traversal(current_node, count)
+  #   return current_node if current_node.next.nil?
+
+  #   count.times do
+  #     current_node = current_node.next
+  #   end
+
+  #   current_node
+  # end
+
+  # def swatch(current, node)
+  #   temp = current.next
+  #   current.next = node
+  #   node.next = temp
+  # end
+end
